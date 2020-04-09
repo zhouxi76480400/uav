7d3bae5d95c0b00c4a0a1e92a1e35bcac24e492b
zhouxi <zhouxi121@live.com>
2020-04-09 15:17:36 +0900

    test

---
+++ M210VideoStreamReader/cmake-build-debug/CMakeFiles/M210VideoStreamReader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/M210VideoStreamReader.dir/main.cpp.o"
+  "M210VideoStreamReader"
+  "M210VideoStreamReader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/M210VideoStreamReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
